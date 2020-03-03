0f3bc5afd5c61db90e0a40ad5a15355664b7efab
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-03-03 12:32:11 +0700

    memahami Rounting Request

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers);
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first Page</title><head>');
-    res.write('<body><h1>Hello from my node.js  Server</h1></body>');
-    res.write('</html>');
-    res.end();
+	console.log(req.url, req.method, req.headers);
+	const url = req.url;
+	if (url === '/') {
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html>');
+		res.write('<head><title>Enter Message</title><head>');
+		res.write(`
+			<body>
+			<h1>Rounting Request</h1>
+                <form action="/message" method="post">
+                    <input type="text" name="message" id="">
+                    <button type="submit">Send</button>
+                </form>
+            </body>`);
+		res.write('</html>');
+		return res.end();
+	}
+	res.setHeader('Content-Type', 'text/html');
+	res.write('<html>');
+	res.write('<head><title>My first Page</title><head>');
+	res.write('<body><h1>Hello from my node.js  Server</h1></body>');
+	res.write('</html>');
+	res.end();
 });
 
 server.listen(3000);
