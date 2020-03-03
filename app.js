c7da04249424a0de1e74b9adee14082c53c8a375
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-03-03 12:43:57 +0700

    cara Redirecting Request

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 	console.log(req.url, req.method, req.headers);
 	const url = req.url;
+	const method = req.method;
 	if (url === '/') {
 		res.setHeader('Content-Type', 'text/html');
 		res.write('<html>');
 		res.write('<head><title>Enter Message</title><head>');
 		res.write(`
 			<body>
-			<h1>Rounting Request</h1>
+			<h1>Input your message</h1>
                 <form action="/message" method="post">
                     <input type="text" name="message" id="">
                     <button type="submit">Send</button>
@@ -18,6 +20,14 @@ const server = http.createServer((req, res) => {
 		res.write('</html>');
 		return res.end();
 	}
+
+	if (url == '/message' && method == 'POST') {
+		fs.writeFileSync('message.txt', 'DUMMY');
+		res.statusCode = 302;
+		res.setHeader('Location', '/');
+		return res.end();
+	}
+
 	res.setHeader('Content-Type', 'text/html');
 	res.write('<html>');
 	res.write('<head><title>My first Page</title><head>');
