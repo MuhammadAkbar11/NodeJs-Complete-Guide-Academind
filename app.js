907151f45f3591bdf065d0f005a7bef22faca2dd
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-03-07 16:37:53 +0700

    cara kerja express.js di belakang layar dan menangani routes yang berbeda

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const http = require('http');
+// const http = require('http');
 
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-	console.log('in The Midddleware');
-	next(); //* Allows the request to continue to the next moddleeware in line
+// app.use((req, res, next) => {
+// 	console.log('in The Midddleware');
+// 	next(); //* Allows the request to continue to the next moddleeware in line
+// });
+
+app.use('/add-product', (req, res, next) => {
+	console.log('in another Midddleware');
+	res.send(`<h1>the "add product " page</h1>`);
 });
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
 	console.log('in another Midddleware');
 	res.send(`<h1>Hello from Express.js</h1>`);
 });
 
-const server = http.createServer(app);
+app.listen(5050);
 
-server.listen(5000);
+// const server = http.createServer(app);
+
+// server.listen(5000);
