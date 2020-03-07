9454171d9025118a25d2f848c22b0d168db4d7b9
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-03-07 16:24:16 +0700

    cara kerja Middleware

--- app.js
+++ app.js
@@ -6,12 +6,13 @@ const app = express();
 
 app.use((req, res, next) => {
 	console.log('in The Midddleware');
-	next();
+	next(); //* Allows the request to continue to the next moddleeware in line
 });
 app.use((req, res, next) => {
 	console.log('in another Midddleware');
+	res.send(`<h1>Hello from Express.js</h1>`);
 });
 
 const server = http.createServer(app);
 
-server.listen(3000);
+server.listen(5000);
