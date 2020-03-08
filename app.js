3950dd8fe7d2c0142e632b9f59c176ac6feafdb2
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-03-08 13:33:53 +0700

    mengembalikan halaman 404

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -11,10 +12,10 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).send(`<h1>Page Not Found</h1>`);
+	res.status(404).sendfile(path.join(__dirname, 'views', '404.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5050;
 
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
 
