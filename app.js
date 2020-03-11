5c9e5c78fe69a00de79a815905e9ff367421eb40
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-03-11 15:16:18 +0700

    membuat output yang dinamis dan mengkonversi file .HTML ke file .pug

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
 
 app.set('view engine', 'pug');
@@ -16,7 +18,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-	res.status(404).sendfile(path.join(__dirname, 'views', '404.html'));
+	res.status(404).render('404', { pageTitle: 'Page Not Found 404' });
 });
 
 const PORT = process.env.PORT || 5050;
--- routes/admin.js
+++ routes/admin.js
@@ -1,5 +1,6 @@
-const express = require('express');
 const path = require('path');
+
+const express = require('express');
 
 const rootDir = require('../util/path');
 
@@ -7,14 +8,14 @@ const router = express.Router();
 
 const products = [];
 
-// => admin/add-product => GET
+//=> /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-	res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+	res.render('add-product', { pageTitle: 'Add Product' });
 });
 
-// => admin/add-product => POST
+//=> /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-	console.log(req.body);
+	products.push({ title: req.body.title, price: req.body.price });
 	res.redirect('/');
 });
 
