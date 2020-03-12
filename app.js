eee93a204ed20941a4dfd89df16eb19dfd694830
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-03-12 13:08:15 +0700

    end of pug section

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+const users = [];
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res, next) => {
-	res.render();
+	res.render('pug/index', { pageTitle: 'Home', users: users });
 });
 
 app.get('/users', (req, res, next) => {
-	res.render();
+	res.render('pug/users', { pageTitle: 'Add Users Page', users: users });
 });
 
-app.post('/add-uses', (req, res, next) => {
-	res.redirect('/');
+app.post('/add-users', (req, res, next) => {
+	users.push({ name: req.body.username });
+	res.redirect('/users');
 });
 
 app.listen(8080);
