cbd10c94566ac1169d5fc9d5caa5ffb206ce7a8c
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-03-14 14:31:02 +0700

    menyalin file2  dari folder modul sebelumnya

---
+++ app.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+const PORT = process.env.PORT || 5050;
+
+app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
+
+// app.listen(5050);
---
+++ routes/shop.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+	const products = adminData.products;
+	res.render('home', {
+		prods: products,
+		pageTitle: 'Home | Shop.com',
+		path: '/',
+		hasProducts: products.length > 0,
+		activeShop: true,
+		productCSS: true
+	});
+});
+
+router.get('/boot', (req, res, next) => {
+	res.render('layout/bootstrap-layout', { pageTitle: 'Test Bootstrap' });
+});
+module.exports = router;
