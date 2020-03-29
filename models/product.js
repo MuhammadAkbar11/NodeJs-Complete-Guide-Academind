bffc363e35b7008c9a7bbcc1ac70b0088cf9cd9a
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2020-03-29 13:38:12 +0700

    membuat model cart

--- models/product.js
+++ models/product.js
@@ -1,36 +1,35 @@
 const fs = require('fs');
 const path = require('path');
 
-const pathData = path.join(
+const p = path.join(
 	path.dirname(process.mainModule.filename),
 	'data',
 	'products.json'
 );
 
 const getProductsFromFile = cb => {
-	fs.readFile(pathData, (err, res) => {
+	fs.readFile(p, (err, fileContent) => {
 		if (err) {
-			return cb([]);
+			cb([]);
 		} else {
-			cb(JSON.parse(res));
+			cb(JSON.parse(fileContent));
 		}
 	});
 };
 
 module.exports = class Product {
-	constructor(title, price, imageUrl, description) {
+	constructor(title, imageUrl, description, price) {
 		this.title = title;
-		this.price = price;
 		this.imageUrl = imageUrl;
 		this.description = description;
+		this.price = price;
 	}
 
 	save() {
-		// products.push(this);
 		this.id = Math.random().toString();
 		getProductsFromFile(products => {
 			products.push(this);
-			fs.writeFile(pathData, JSON.stringify(products), err => {
+			fs.writeFile(p, JSON.stringify(products), err => {
 				console.log(err);
 			});
 		});
@@ -38,7 +37,6 @@ module.exports = class Product {
 
 	static fetchAll(cb) {
 		getProductsFromFile(cb);
-		// return products;
 	}
 
 	static findById(id, cb) {
