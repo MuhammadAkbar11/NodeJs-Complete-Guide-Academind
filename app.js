0558841bdefc8ef4e09bcb7a130179ec02276f12
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-02-20 11:52:52 +0700

    sedikit perubahan

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ const bodyParser = require('body-parser');
 const app = express();
 const users = [];
 
-app.set('view engine', 'pug');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', (req, res, next) => {
-	res.render('pug/index', { pageTitle: 'Home', users: users });
+	res.render('ejs/index', { pageTitle: 'Home', users: users });
 });
 
 app.get('/users', (req, res, next) => {
-	res.render('pug/users', { pageTitle: 'Add Users Page', users: users });
+	res.render('ejs/users', { pageTitle: 'Add Users Page', users: users });
 });
 
 app.post('/add-users', (req, res, next) => {
