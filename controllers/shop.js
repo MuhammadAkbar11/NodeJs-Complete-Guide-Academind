531e713903f9a755dab72f93e4fe4d61b4ca0ec9
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-02-26 10:18:31 +0700

    mengambil semua data produk di halaman utama

--- controllers/shop.js
+++ controllers/shop.js
@@ -29,7 +29,7 @@ exports.getProducts = (req, res, next) => {
 // };
 
 exports.getIndex = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then(products => {
       res.render("shop/index", {
         prods: products,
