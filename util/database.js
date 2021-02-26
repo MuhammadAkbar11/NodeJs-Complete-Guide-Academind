89b2af33e5b1eff4ee5c0c1c1b0bee0a9f7bae9e
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-02-26 10:17:53 +0700

    fix timeout connetion

--- util/database.js
+++ util/database.js
@@ -1,13 +1,16 @@
 const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 const uri =
-  "mongodb+srv://akbar:akbar2001@bae2001.7m1a3.mongodb.net/phoenix-shop?retryWrites=true&w=majority";
+  "mongodb+srv://akbar:akbar2001@bae2001.7m1a3.mongodb.net/phoenix-shop?retryWrites=true&w=majority&ssl=true";
+
+const url2 =
+  "mongodb://phoenix:phoenix2001@bae2001-shard-00-00.7m1a3.mongodb.net:27017,bae2001-shard-00-01.7m1a3.mongodb.net:27017,bae2001-shard-00-02.7m1a3.mongodb.net:27017/phoenix-shop?ssl=true&replicaSet=atlas-kyz0lz-shard-0&authSource=admin&retryWrites=true&w=majority";
 
 let _db;
 
 const mongoConnect = () => {
   return new Promise((resolve, reject) => {
-    return MongoClient.connect(uri, {
+    return MongoClient.connect(url2, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
