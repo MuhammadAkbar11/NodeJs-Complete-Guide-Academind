972460f1cb6ffdd07139bfc2a5b1367e13945c35
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-02-28 02:20:08 +0700

    menghubungkan server dengan mongoose

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const path = require("path");
 const express = require("express");
 require("dotenv").config();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const errorController = require("./controllers/error");
-const mongoConnect = require("./util/database").mongoConnect;
 
 const UserModel = require("./models/user");
 
@@ -14,37 +14,40 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // eslint-disable-next-line no-undef
 app.use(express.static(path.join(__dirname, "public")));
 // eslint-disable-nMongoDBConnect
-app.use((req, res, next) => {
-  UserModel.findById("6038aa625ed7c15820af6a2d")
-    .then(user => {
-      req.user = new UserModel(user.username, user.email, user._id, user.cart);
-      next();
-    })
-    .catch(err => console.log(err));
-});
+// app.use((req, res, next) => {
+//   UserModel.findById("6038aa625ed7c15820af6a2d")
+//     .then(user => {
+//       req.user = new UserModel(user.username, user.email, user._id, user.cart);
+//       next();
+//     })
+//     .catch(err => console.log(err));
+// });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 1996;
+const URL = process.env.URL;
 
-mongoConnect()
+mongoose
+  .connect(URL)
   .then(result => {
-    // console.log("connected to mongodb", result);
     app.listen(PORT, () => {
       console.log("listening... PORT " + PORT);
     });
   })
   .catch(err => {
-    console.log(err, "errors");
+    console.log(err);
   });
+
+console.log(process.env.URL);
