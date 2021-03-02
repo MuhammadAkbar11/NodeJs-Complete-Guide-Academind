12465a66f522f75781f6ebadf9837e18ad155832
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-02 11:15:20 +0700

    Menggunakan mongDB untuk  Store Session

--- app.js
+++ app.js
@@ -5,11 +5,18 @@ require("dotenv").config();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 const errorController = require("./controllers/error");
 const UserModel = require("./models/userModel");
 
+const MONGODB_URI = process.env.URL;
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: "sessions",
+});
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -23,7 +30,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
-  session({ secret: "phoenix secret", resave: false, saveUninitialized: false })
+  session({
+    secret: "phoenix secret",
+    resave: false,
+    saveUninitialized: false,
+    store: store,
+  })
 );
 
 app.use((req, res, next) => {
@@ -45,10 +57,9 @@ app.use(errorController.get404);
 
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 3000;
-const URL = process.env.URL;
 
 mongoose
-  .connect(URL)
+  .connect(MONGODB_URI)
   .then(result => {
     UserModel.findOne().then(user => {
       if (!user) {
