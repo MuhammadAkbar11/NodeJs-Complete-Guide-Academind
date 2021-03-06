23de4e37bf3e0dd5d71c633b339db70c8a47f557
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-06 09:52:52 +0700

    fungsi util untuk mengirim email

---
+++ util/sendMail.js
@@ -0,0 +1,44 @@
+const nodemailer = require("nodemailer");
+
+const path = require("path");
+const ejs = require("ejs");
+const fs = require("fs");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAILPW,
+  },
+});
+
+const mailOptions = {
+  fromName: "",
+  fromEmail: "",
+  to: "",
+  subject: "",
+  message: "",
+  html: ``,
+};
+
+const sendMail = async (options = mailOptions) => {
+  const file = fs.readFileSync(
+    path.resolve(__dirname, "../views/email-verification.ejs"),
+    "ascii"
+  );
+  const rendered = ejs.render(file, { text: "haii" });
+
+  const message = {
+    from: `${options.fromName} phoenix.production98@gmail.com`,
+    to: options.to,
+    subject: options.subject,
+    text: options.message,
+    html: rendered,
+  };
+
+  const info = await transporter.sendMail(message);
+
+  return info;
+};
+
+module.exports = sendMail;
