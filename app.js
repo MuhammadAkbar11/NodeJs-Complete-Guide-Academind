516cb742a991379a552d7323f3e65e0807e1455b
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-28 23:35:35 +0700

    menyiapkan starter proyek

---
+++ app.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log("APIs listening... PORT " + PORT);
+});
