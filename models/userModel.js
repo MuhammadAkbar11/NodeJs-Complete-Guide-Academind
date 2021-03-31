5663ef3127cfd808aa1b7490b6d2298482b19f1c
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-31 21:32:49 +0700

    membuat user model

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    posts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "PostModel",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+const UserModel = mongoose.model("UserModel", userSchema, "users");
+
+module.exports = UserModel;
