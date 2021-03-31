d202de46aa7458e55b646925cd32808cbb554a87
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-03-31 22:57:46 +0700

    menyelesaikan proses signup

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,20 +1,47 @@
+const bcrypt = require("bcryptjs");
 const { validationResult } = require("express-validator");
+const UserModel = require("../models/userModel");
 const errMessageValidation = require("../utils/errMessageValidation");
 
-exports.postSignUp = (req, res, next) => {
+exports.postSignUp = async (req, res, next) => {
   const name = req.body.name;
   const email = req.body.email;
   const password = req.body.password;
-  const confirmPassword = req.body.password2;
 
   const errors = validationResult(req);
   const errorMsgs = errMessageValidation(errors.array());
 
-  if (!errors.isEmpty()) {
-    const error = new Error();
-    error.message = "Validation failed , entered data is incorrect";
-    error.statusCode = 422;
-    error.validationMessage = errorMsgs;
-    throw error;
+  try {
+    if (!errors.isEmpty()) {
+      const error = new Error("Validation failed, entered data is incorrect");
+      error.statusCode = 422;
+      error.data = {
+        validationMessage: errorMsgs,
+      };
+      throw error;
+    }
+
+    const hashedPw = await bcrypt.hash(password, 12);
+    const user = new UserModel({
+      email: email,
+      name: name,
+      password: hashedPw,
+    });
+
+    const result = await user.save();
+
+    return res.status(200).json({
+      status: "success",
+      message: "success create account",
+      user: result,
+      request: { name, email, password },
+    });
+  } catch (error) {
+    console.log(error, "error auth");
+    if (!error.statusCode) {
+      error.statusCode = 500;
+      error.message = "Something went wrong";
+    }
+    next(error);
   }
 };
--- models/userModel.js
+++ models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = new Schema(
     },
     status: {
       type: String,
-      required: true,
+      default: "new user",
     },
     posts: [
       {
