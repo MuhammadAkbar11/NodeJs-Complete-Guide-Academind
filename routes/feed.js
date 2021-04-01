cecad2cec94dd2202a0a67473db862021d7ef880
MuhammadAkbar11 <muhammadakbarletlet@gmail.com>
2021-04-02 00:34:09 +0700

    validasi untuk token

--- routes/feed.js
+++ routes/feed.js
@@ -1,16 +1,16 @@
 const express = require("express");
 
 const feedController = require("../controllers/feed");
-
+const isAuth = require("../middleware/is-auth");
 const postValidation = require("../middleware/validators/feed");
 
 const router = express.Router();
 
 // GET /feed/posts
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 // POST /feed/post
-router.post("/post", [postValidation], feedController.createPost);
+router.post("/post", isAuth, [postValidation], feedController.createPost);
 
 router.get("/post/:postId", feedController.getPost);
 
